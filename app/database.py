f1553653ee73eb5a556f624ce6c23fdff1631d30
qdelaplebe-lgtm <qdelaplebe@gmail.com>
2026-02-13 10:08:56 +0000

    Update import path for settings in database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import settings  # Assurez-vous que le chemin est correct : .config ou ..config selon votre structure
-
+from core.config import settings
 # --- CONNEXION À LA BASE DE DONNÉES ---
 DATABASE_URL = settings.DATABASE_URL
 
