46b14772ffa5d5d58a7400e28b8c9a0bd0006824
qdelaplebe-lgtm <qdelaplebe@gmail.com>
2026-02-13 13:30:34 +0000

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -23,6 +23,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 jours
+    
+    # Répertoire pour les uploads locaux (si utilisé)
+    UPLOAD_DIR: str = "/tmp/uploads"
 
     class Config:
         case_sensitive = True
